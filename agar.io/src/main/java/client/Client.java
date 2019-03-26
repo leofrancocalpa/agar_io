b4422371419a392f2a99c493d9923aed37741086
Leonardo Franco Calpa <35975867+leofrancocalpa@users.noreply.github.com>
2019-03-26 00:33:55 -0500

    Update comments

--- agar.io/src/main/java/client/Client.java
+++ agar.io/src/main/java/client/Client.java
@@ -69,14 +69,16 @@ public class Client extends Thread {
 	}
 	/**
 	 * 
-	 * @return  Array of strings that represent the position x,y and mass of every food in the game
+	 * @return  Array of strings that represent the position x,y and mass (width and height) of every food in the game
+	 * Example : food[0] -> x,y,w,h
 	 */
 	public String[] getFoodFromGame() {
 		return null;
 	}
 	/**
 	 * 
-	 * @return Array of strings that represent the position x,y and mass of every player in the game
+	 * @return Array of strings that represent the position x,y and mass (width and height) of every player in the game
+	 * Example : player[0] -> x,y,w,h,id
 	 */
 	public String[] getPlayerFromGame() {
 		return null;
@@ -85,6 +87,7 @@ public class Client extends Thread {
 	 * 
 	 * @return Array of strings that represent the initial info of the user. Initial position, initial mass
 	 * (width and height) and id
+	 * infoPlayer[0]-> x , infoPlayer[1]-> y, infoPlayer[2]-> w, infoPlayer[3]-> h, infoPlayer[4]-> id
 	 */
 	public String[] getInfoPlayer() {
 		return null;
